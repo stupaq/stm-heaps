a2c79c71dfee565d448cdad265491479013d6dd7
Mateusz Machalica <mateuszmachalica@gmail.com>
2014-05-28 23:30:26 +0200

    Proved inserting (not tested).

--- FineHeap.hs
+++ FineHeap.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE TupleSections     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 module FineHeap where
 
@@ -26,66 +27,35 @@ readNode :: FineHeap e -> STM (Int, Maybe e)
 readNode (FineHeap size elem _ _) = liftM2 (,) (readTVar size) (readTVar elem)
 readNode Nil = return (0, Nothing)
 
-reserveNode :: FineHeap e -> STM ()
-reserveNode (FineHeap size _ _ _) = readTVar size >>= writeTVar size . (+1)
+putElem :: (Ord e) => FineHeap e -> e -> STM (Maybe e)
+putElem root@(FineHeap size elem _ _) new = do
+  (sz, el) <- readNode root
+  case (sz, el) of
+    (0, Nothing) -> do
+      -- This is an empty node, we can put our stuff here and finish
+      writeTVar elem $ Just new
+      return Nothing
+    (_, Nothing) ->
+      -- This is a gap, we'll wait for someone to get rid of it
+      retry
+    (_, Just el) -> do
+      -- We swap the element to be inserted if necessary
+      if new < el
+      then writeTVar elem (Just new) >> return (Just el)
+      else return (Just new)
 
-putElem :: (Ord e) => FineHeap e -> e -> IO ()
-putElem root@(FineHeap _ elem left right) new = do
+putElemRec :: (Ord e) => FineHeap e -> e -> IO ()
+putElemRec (FineHeap _ _ left right) new = do
   next <- atomically $ do
-    (sz, el) <- readNode root
-    -- The size has already been updated by caller
-    case (sz, el) of
-      (1, Nothing) -> do
-        -- This is the dangling root node when the heap is empty
-        writeTVar elem $ Just new
-        return Nothing
-      (_, Nothing) ->
-        -- This is a gap and it's not ours, we'll wait for someone to get rid of it
-        retry
-      (_, Just el) -> do
-        -- We swap the element to be inserted if necessary
-        new' <- if new < el then writeTVar elem (Just new) >> return el else return new
-        -- We reserve place in proper subtree
-        root' <- chooseChild
-        reserveNode root'
-        return $ Just (root', new')
+    -- We choose the smaller subtree for insertion
+    [lsz, rsz] <- mapM (readTVar . heapSize) [left, right]
+    let root = if lsz < rsz then left else right
+    -- Update it
+    fmap (root, ) <$> putElem root new
+  -- And proceed recursively if necessary
   case next of
     Nothing -> return ()
-    Just (root', new') -> putElem root' new'
-    where
-      chooseChild = do
-        [lsz, rsz] <- mapM (readTVar . heapSize) [left, right]
-        return $ if lsz < rsz then left else right
-
-instance (Ord e) => ConcurrentHeap (FineHeap e) e where
-  heapNew n
-    | n > 0 = do
-      s <- newTVarIO 0
-      e <- newTVarIO Nothing
-      let m = n `quot` 2 -- ceil((n - 1) / 2)
-      [tl, tr] <- replicateM 2 $ heapNew m
-      return $ FineHeap s e tl tr
-    | otherwise = return Nil
-
-  heapPut root@(FineHeap size _ _ _) new = do
-    atomically $ do
-      sz <- readTVar size
-      writeTVar size (sz + 1)
-      -- TODO
-
-  heapPop root@(FineHeap size elem left right) = do
-    el <- atomically $ do
-      sz <- readTVar size
-      check $ sz > 0
-      writeTVar size (sz - 1)
-      el <- readTVar elem
-      case el of
-        Nothing -> retry
-        Just el -> do
-          writeTVar elem Nothing
-          return el
-    pushGap root
-    return el
+    Just (root', new') -> putElemRec root' new'
 
 pushGap :: (Ord e) => FineHeap e -> IO ()
 pushGap (FineHeap size elem left right) = do
@@ -110,6 +80,36 @@ pushGap (FineHeap size elem left right) = do
     Nothing -> return ()
     Just tree -> pushGap tree
 
+instance (Ord e) => ConcurrentHeap (FineHeap e) e where
+  heapNew n
+    | n > 0 = do
+      s <- newTVarIO 0
+      e <- newTVarIO Nothing
+      let m = n `quot` 2 -- ceil((n - 1) / 2)
+      [tl, tr] <- replicateM 2 $ heapNew m
+      return $ FineHeap s e tl tr
+    | otherwise = return Nil
+
+  heapPut root@(FineHeap {}) new = do
+    next <- atomically $ putElem root new
+    case next of
+      Nothing -> return ()
+      Just new' -> putElemRec root new'
+
+  heapPop root@(FineHeap size elem left right) = do
+    el <- atomically $ do
+      sz <- readTVar size
+      check $ sz > 0
+      writeTVar size (sz - 1)
+      el <- readTVar elem
+      case el of
+        Nothing -> retry
+        Just el -> do
+          writeTVar elem Nothing
+          return el
+    pushGap root
+    return el
+
 testSorting :: [Int] -> Property
 testSorting xs = monadicIO $ do
   sxs <- run $ do
@@ -119,5 +119,6 @@ testSorting xs = monadicIO $ do
     replicateM l (heapPop h)
   Test.QuickCheck.Monadic.assert $ sxs == sort xs
 
+testIt :: IO ()
 testIt = quickCheck testSorting
 
