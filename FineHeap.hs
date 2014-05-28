ae2441039d93f35176edcb8e17d06302a0f1cadf
Mateusz Machalica <mateuszmachalica@gmail.com>
2014-05-29 00:38:59 +0200

    Code ready.

--- FineHeap.hs
+++ FineHeap.hs
@@ -32,14 +32,14 @@ putElem root@(FineHeap size elem _ _) new = do
   (sz, el) <- readNode root
   case (sz, el) of
     (0, Nothing) -> do
-      -- This is an empty node, we can put our stuff here and finish
+      -- This is an empty node, we can put our stuff here and finish.
       writeTVar elem $ Just new
       return Nothing
     (_, Nothing) ->
-      -- This is a gap, we'll wait for someone to get rid of it
+      -- This is a gap, we'll wait for someone to get rid of it.
       retry
     (_, Just el) -> do
-      -- We swap the element to be inserted if necessary
+      -- We swap the element to be inserted if necessary.
       if new < el
       then writeTVar elem (Just new) >> return (Just el)
       else return (Just new)
@@ -47,38 +47,62 @@ putElem root@(FineHeap size elem _ _) new = do
 putElemRec :: (Ord e) => FineHeap e -> e -> IO ()
 putElemRec (FineHeap _ _ left right) new = do
   next <- atomically $ do
-    -- We choose the smaller subtree for insertion
+    -- We choose the smaller subtree for insertion, note that we do not
+    -- care whether the children have gaps -- we drive our decision based
+    -- on size only.
     [lsz, rsz] <- mapM (readTVar . heapSize) [left, right]
     let root = if lsz < rsz then left else right
-    -- Update it
+    -- Update it.
     fmap (root, ) <$> putElem root new
-  -- And proceed recursively if necessary
+  -- And proceed recursively if necessary.
   case next of
     Nothing -> return ()
     Just (root', new') -> putElemRec root' new'
 
-pushGap :: (Ord e) => FineHeap e -> IO ()
-pushGap (FineHeap size elem left right) = do
+putGap :: FineHeap e -> STM e
+putGap root@(FineHeap size elem left right) = do
+  (sz, el) <- readNode root
+  writeTVar size (sz - 1)
+  check $ sz > 0
+  case el of
+    Nothing ->
+      -- This is a gap, we'll wait for someone to push it down.
+      retry
+    Just el ->
+      -- We create a gap, we'll deal with it in a moment.
+      writeTVar elem Nothing >> return el
+
+lowerGap :: (Ord e) => FineHeap e -> IO ()
+lowerGap root@(FineHeap _ elem left right) = do
   next <- atomically $ do
-    -- size has been decremented by the caller
-    sz <- readTVar size
-    el <- readTVar elem
+    -- The size has been decremented by someone who put a gap here.
+    (sz, el) <- readNode root
     case (sz, el) of
-      (0, _) -> return Nothing
+      (0, _) ->
+        -- If it's empty then we're done.
+        return Nothing
       (_, Nothing) -> do
-        [lel, rel] <- mapM (readTVar . heapElem) [left, right]
-        case (lel, rel) of
-          -- FIXME
-          (Nothing, _) -> retry
-          (_, Nothing) -> retry
-          (Just lel, Just rel) ->
-            let (tree, el) = minimumBy (comparing snd) [(left, lel), (right, rel)] in do
-              writeTVar elem $ Just el
-              writeTVar (heapElem tree) Nothing
-              return $ Just tree
+        -- Otherwise choose a min child.
+        children <- mapM readNode [left, right]
+        case children of
+          -- In these cases we have no choice.
+          [(0, Nothing), (_, Just rel)] -> swapGap right rel
+          [(_, Just lel), (0, Nothing)] -> swapGap left lel
+          -- Otherwise, we hold back if any of the nodes is a gap.
+          [(_, Nothing), _] -> retry
+          [_, (_, Nothing)] -> retry
+          -- Finally, we swap with min child
+          [(lsz, Just lel), (rsz, Just rel)] ->
+            let (tree, el) = minimumBy (comparing snd) [(left, lel), (right, rel)]
+            in swapGap tree el
   case next of
     Nothing -> return ()
-    Just tree -> pushGap tree
+    Just tree -> lowerGap tree
+    where
+      swapGap tree el = do
+        writeTVar elem $ Just el
+        writeTVar (heapElem tree) Nothing
+        return $ Just tree
 
 instance (Ord e) => ConcurrentHeap (FineHeap e) e where
   heapNew n
@@ -91,23 +115,18 @@ instance (Ord e) => ConcurrentHeap (FineHeap e) e where
     | otherwise = return Nil
 
   heapPut root@(FineHeap {}) new = do
+    putStrLn $ "put"
     next <- atomically $ putElem root new
     case next of
       Nothing -> return ()
       Just new' -> putElemRec root new'
+    putStrLn $ "put end"
 
-  heapPop root@(FineHeap size elem left right) = do
-    el <- atomically $ do
-      sz <- readTVar size
-      check $ sz > 0
-      writeTVar size (sz - 1)
-      el <- readTVar elem
-      case el of
-        Nothing -> retry
-        Just el -> do
-          writeTVar elem Nothing
-          return el
-    pushGap root
+  heapPop root@(FineHeap {}) = do
+    putStrLn $ "pop"
+    el <- atomically $ putGap root
+    lowerGap root
+    putStrLn $ "pop end"
     return el
 
 testSorting :: [Int] -> Property
